884a65673e9a81f65c3f98937c786b078b2a35ab
Todd Dole <todd.dole@gmail.com>
2024-02-16 12:09:17 -0600

    Eigth Commit - app v0.1 update

--- src/app.py
+++ src/app.py
@@ -1,27 +1,57 @@
 #!/usr/bin/env python3
 
-# by Todd Dole
-# MVP web application for Software Architecture project
-# Prompts user to input a latitude and longitude
-# Then displays the location on a google map
+# V 0.1 gets race list and displays most recent race and start list
 
 from flask import Flask, request, url_for, render_template
 from markupsafe import escape
+from components.LocationDataGateway import LocationDataGateway
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return '''
-     <form action="/echo_user_input" method="POST">
-         Latitude:<input name="latitude">
-	 Longitude:<input name="longitude">
-         <input type="submit" value="Submit!">
-     </form>
-     '''
+    ldg = LocationDataGateway()
+    racelist = ldg.get_all_data("racelist")
+    if (len(racelist)==0):
+        return '''<h1>No Races Found!  Come Back Later!</h1>'''
 
-@app.route("/echo_user_input", methods=["POST"])
-def echo_input():
-    input_lat = request.form.get("latitude", "")
-    input_long = request.form.get("longitude", "")
-    return render_template('map.html') + f"<script>initMap( {escape(input_lat)}, {escape(input_long)} );</script> You entered: {escape(input_lat)}, {escape(input_long)}</body></html>"
+    bignum=0
+    rname = ""
+    for r in racelist:
+        rnum = int(r["_id"])
+        if (rnum>bignum):
+            bignum = rnum
+            rname = r["data"]
+    rname = rname["data"]
+
+    returnstr = "<h1>Today's Race: " + rname + "</h1>\n"
+    returnstr += "<h2>Starters:</h2>\n"
+
+    rname = "race"+rname
+    racenumbers = ldg.get_all_data(rname)
+    racenumbers = racenumbers[0]["data"]
+
+    athletes = ldg.get_all_data("athletes")
+
+    returnstr+="<Table><tr><th>Race Number</th><th>Name</th><th>division</th></tr>\n"
+
+
+    for key in racenumbers:
+        returnstr += "<tr><td>"+str(key)+"</td><td>"
+        athid = int(racenumbers[key])
+
+        for athletecount in range(len(athletes)):
+            if (int((athletes[athletecount])["_id"]) == athid): break
+        athlete = athletes[athletecount]["data"]
+
+
+        athlete = json.loads(athlete)
+        returnstr+=""+athlete["name"] + "</td><td>"+str(athlete["division"])+"</td></tr>\n"
+
+    returnstr += "</table>\n"
+
+    return returnstr
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
